1b8c02c0a059732dfa1f74244034046864df70cd
Kris Vukasinovic <axellerate@hotmail.com>
2017-02-03 16:18:02 -0500

    added keywords and run early testing

--- main.py
+++ main.py
@@ -10,10 +10,26 @@ from ArticleAnalyzer import ArticleAnalyzer
 with open('test_data/arctic_article.txt', 'r') as myfile:
 	arctic_article = myfile.read().replace('\n', '')
 
+with open('test_data/arctic_article2.txt', 'r') as myfile:
+	arctic_article2 = myfile.read().replace('\n', '')
+
 with open('test_data/regular_article.txt', 'r') as myfile:
 	regular_article = myfile.read().replace('\n', '')
 
-article_preprocessor = ArticlePreprocessor(regular_article)
+with open('test_data/forest_article.txt', 'r') as myfile:
+	forest_article = myfile.read().replace('\n', '')
+
+with open('test_data/oceans_article.txt', 'r') as myfile:
+	oceans_article = myfile.read().replace('\n', '')
+
+with open('test_data/oceans_article2.txt', 'r') as myfile:
+	oceans_article2 = myfile.read().replace('\n', '')
+
+with open('test_data/air_article.txt', 'r') as myfile:
+	air_article = myfile.read().replace('\n', '')
+
+
+article_preprocessor = ArticlePreprocessor(oceans_article2)
 tagged_words = article_preprocessor.tagged_article
 chunked_words = article_preprocessor.chuck_words(tagged_words)
 
